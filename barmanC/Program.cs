3944b5a28ce359863fa3a129f0bd74f0ec629386
Vojtech Gadurek <gadurekvojtech@outlook.com>
2022-10-08 12:13:18 +0200

    funkcní

--- barmanC/Program.cs
+++ barmanC/Program.cs
@@ -1,8 +1,18 @@
 ﻿using System.Diagnostics;
 using System.Reflection.Metadata;
 
-bool debug = true;
-double[] teams = new double[]{0,0,0,0};
+string zaloha = "zaloha.txt";
+/*if (File.Exists(zaloha))
+{
+    throw new Exception("Soubor " + zaloha + " již existuje.");
+}
+*/
+
+//File.Create(zaloha).Close();
+
+
+bool debug = false;
+double[] teams = new double[]{0,0,0,0,0,0};
 
 double score = 0;
 string[] ingredients = new string[]
@@ -23,12 +33,14 @@ foreach (var thing in ingredients)
 Combos comba = new Combos();
 
 
-IDictionary<string, int> annanasovy_drink = new Dictionary<string, int>();
-annanasovy_drink.Add("ananas", 10);
-annanasovy_drink.Add("tatarka", 20);
-annanasovy_drink.Add("kofola", 70);
+IDictionary<string, int> annanasova_kolaloka = new Dictionary<string, int>();
+annanasova_kolaloka.Add("ananas", 10);
+annanasova_kolaloka.Add("horka_voda", 20);
+annanasova_kolaloka.Add("kofola", 70);
+annanasova_kolaloka.Add("tatarka", 10);
+annanasova_kolaloka.Add("led", 20);
 
-comba.stuff.Add("annanasovy_drink", new Combo(300, annanasovy_drink, 20000));
+comba.stuff.Add("annanasova_kolaloka", new Combo(300, annanasova_kolaloka, 30000));
 
 IDictionary<string, int> tepla_kofola = new Dictionary<string, int>();
 
@@ -43,7 +55,7 @@ apfel_kraut.Add("most", 30);
 apfel_kraut.Add("horka_voda", 70);
 apfel_kraut.Add("led", 30);
 
-comba.stuff.Add("apfel_kraut", new Combo(300, apfel_kraut, 20000));
+comba.stuff.Add("apfel_kraut", new Combo(300, apfel_kraut, 40000));
 
 IDictionary<string, int> svarena_kofola = new Dictionary<string, int>();
 svarena_kofola.Add("kofola", 140);
@@ -62,7 +74,10 @@ while (true)
     bool correct_input = true;
     try
     {
-      team_number = Int32.Parse(Console.ReadLine());
+        string line = Console.ReadLine();
+        File.AppendAllText(zaloha, line + "\n");
+        team_number = Int32.Parse(line);
+        Console.WriteLine(teams[team_number]);
     }
     catch
     {
@@ -82,14 +97,16 @@ while (true)
             int[] data = new int[] { };
             try
             {
-                data = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+                string line = Console.ReadLine();
+                File.AppendAllText(zaloha, line + "\n");
+                data = line.Split(' ').Select(int.Parse).ToArray();
             }
             catch
             {
                 Console.WriteLine("Demente zadal jsi to spatne");
             }
 
-            if (data.Length == 8)
+            if (data.Length == 8 && data.Sum() > 0)
             {
                 IDictionary<string, int> drink = new Dictionary<string, int>();
                 count = 0;
@@ -115,7 +132,7 @@ while (true)
                 if (debug)
                 {
                     foreach (var value in comba.values)
-                     {
+                    {
                         Console.WriteLine(value.Key + " " + value.Value);
                     }
                 }
@@ -150,7 +167,7 @@ record Combos
         }
 
         ansver = values.Sum(x => x.Value);
-        
+        Console.WriteLine($"Crude points:{ansver}");
         return ansver * IceKooficient(drink);
     }
 }
@@ -177,7 +194,7 @@ record Combo(double maxPoints, IDictionary<string, int> order, double maxToGive)
     public double Grade(IDictionary<string, int> drink)
     {
         double fulfilment = findFulfiment(drink);
-        double points = Math.Pow(fulfilment, 2) * maxPoints ;
+        double points = Math.Pow(fulfilment, 1.5) * maxPoints ;
         points *= Math.Pow((maxToGive - givenPoints) / maxToGive, 2);
         int test = (int) fulfilment * 10;
         if (test > 9)
@@ -194,7 +211,21 @@ record Combo(double maxPoints, IDictionary<string, int> order, double maxToGive)
             points *= Math.Pow(test, 1.3);
             bonus -= Math.Pow(test, 1.3);
         }
+
+        
         givenPoints += points;
+        
+        foreach (var thing in order)
+        {
+            if (drink.ContainsKey(thing.Key))
+            {
+                if (drink[thing.Key] > 0)
+                {
+                    points += 1;
+                }
+                
+            }
+        }
         return points;
     }
 }
